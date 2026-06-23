6688fd4f604ba6a8db10ca02850fe54538153738
saadferozee <saadferozee@gmail.com>
2026-06-23 23:49:04 +0600

    [modified] save in local storage feature modified

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,17 @@
 console.log('connected');
 
+// select html objects
 const seatContainer = document.getElementById("seat-container");
 const bookingForm = document.getElementById("booking-form");
 const ticketInput = document.getElementById("ticket-input");
 
-let bookedSeats =
-    JSON.parse(localStorage.getItem("bookedSeats")) || [];
+// variable
+let bookedSeats = JSON.parse(localStorage.getItem("bookedSeats")) || [];
 
-function generateSeats() {
-
+// functions
+const generateSeats = () => {
     const rows = ["A", "B", "C", "D"];
-
     rows.forEach(row => {
-
         for (let i = 1; i <= 25; i++) {
             const seatNumber = row + i;
             const seat = document.createElement("div");
@@ -29,41 +28,38 @@ function generateSeats() {
 
             seatContainer.appendChild(seat);
         }
-
     });
-
+}
+const saveToLocalStorage = (bookedSeats) => {
+    localStorage.setItem('bookedSeats', JSON.stringify(bookedSeats));
 }
 
-generateSeats();
-
+// Listener
 bookingForm.addEventListener("submit", function (event) {
     event.preventDefault();
-
+    
     const seatNumber = ticketInput.value.trim().toUpperCase();
     const seat = document.getElementById(seatNumber);
-
     
     if (!seat) {
         alert("No Seat Selected");
         ticketInput.value = "";
         return;
     }
-
     if (seat.classList.contains("booked")) {
         alert("The seat is already booked.");
         ticketInput.value = "";
         return;
     }
-
+    
     seat.classList.add("booked");
     bookedSeats.push(seatNumber);
-
-    localStorage.setItem(
-        "bookedSeats",
-        JSON.stringify(bookedSeats)
-    );
-
+    
+    saveToLocalStorage(bookedSeats);
+    
     ticketInput.value = "";
-
+    
 });
 
+// calling all function
+generateSeats();
